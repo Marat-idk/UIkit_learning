b43b41d2cfd1c6e3a1037191a8eb3aeb67ca1eb7
Marat-idk <molodoy.dracon@gmail.com>
2022-10-10 05:51:42 +0300

    deviceVC completed

--- ShopApp/ShopApp/Controllers/DeviceViewController.swift
+++ ShopApp/ShopApp/Controllers/DeviceViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 class DeviceViewController: UIViewController {
     
     private var device: Device?
-    
     var deviceNameLabel: UILabel!
     var deviceSegmentedConroller: UISegmentedControl!
     var deviceImageView: UIImageView!
     var deviceImages: [UIImage]?
+    var totalPriceLabel: UILabel!
+
+    
+    let storageLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.text = "Память"
+        lbl.font = .systemFont(ofSize: 22)
+        lbl.textAlignment = .left
+        return lbl
+    }()
+    
+    let storagePicker: UIPickerView = {
+        let pv = UIPickerView()
+        pv.tag = 0
+        return pv
+    }()
+    
+    let priceLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.text = "Стоимость"
+        lbl.font = .systemFont(ofSize: 22)
+        lbl.textAlignment = .left
+        return lbl
+    }()
+        
+    let storageTextField: UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.textAlignment = .center
+        tf.layer.borderColor = UIColor.lightGray.cgColor
+        tf.layer.borderWidth = 1.0
+        tf.layer.cornerRadius = 6.0
+        return tf
+    }()
     
     init(device: Device?, deviceImages: [UIImage]?) {
         self.device = device
@@ -39,13 +74,24 @@ class DeviceViewController: UIViewController {
                 print("storage \(storage) with price \(price)")
             }
         }
-        generateDeviceNameLabel()
-        generateSegmentedControl()
-        generateImageView()
+        
+        // для числа компонент и числа строк в каждой из них
+        storagePicker.dataSource = self
+        // для названия компонент
+        storagePicker.delegate = self
+        
+        generateViews()
         addSubView()
         setConstraints()
     }
     
+    func generateViews() {
+        generateDeviceNameLabel()
+        generateSegmentedControl()
+        generateImageView()
+        generateStoragePicker()
+        generateTotalPriceLabel()
+    }
     func generateDeviceNameLabel() {
         deviceNameLabel = UILabel()
         deviceNameLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -80,16 +126,49 @@ class DeviceViewController: UIViewController {
         }
     }
     
+    func generateStoragePicker() {
+        storageTextField.text = String(device!.storagePrices.keys.first!) + " гб"
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let cancelButton = UIBarButtonItem(title: "cancel", style: .plain, target: self, action: #selector(cancelPicker))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(title: "done", style: .plain, target: self, action: #selector(donePicker(_:)))
+        
+        toolbar.setItems([cancelButton, spaceButton, doneButton], animated: true)
+        storageTextField.inputAccessoryView = toolbar
+        storageTextField.inputView = storagePicker
+    }
+    
+    func generateTotalPriceLabel() {
+        totalPriceLabel = UILabel()
+        totalPriceLabel.translatesAutoresizingMaskIntoConstraints = false
+        let storage = Int(storageTextField.text!.prefix(3))
+        if storage != nil, let price = device?.storagePrices[storage!] {
+            totalPriceLabel.text = String(price) + " руб."
+            totalPriceLabel.font = .boldSystemFont(ofSize: 22)
+            totalPriceLabel.textAlignment = .right
+        }
+    }
+    
     func addSubView() {
         view.addSubview(deviceNameLabel)
         view.addSubview(deviceImageView)
         view.addSubview(deviceSegmentedConroller)
+        view.addSubview(storageLabel)
+        view.addSubview(storageTextField)
+        view.addSubview(priceLabel)
+        view.addSubview(totalPriceLabel)
     }
 
     func setConstraints() {
         setupNameLabel()
         setupImageView()
         setupSegmentedConroller()
+        setupStorageLabel()
+        setupStoragePicker()
+        setupPriceLabel()
+        setupTotalPriceLabel()
     }
     
     func setupNameLabel() {
@@ -100,10 +179,13 @@ class DeviceViewController: UIViewController {
     }
     
     func setupImageView() {
+        deviceImageView.image = deviceImageView.image?.resize(size: CGSize(width: 300, height: 300))
+//        let w = (view.frame.width * 0.3)/view.frame.width
+//        let h = (view.frame.height * 0.3)/view.frame.height
         deviceImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         deviceImageView.topAnchor.constraint(equalTo: deviceNameLabel.bottomAnchor, constant: 20).isActive = true
-        deviceImageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.4).isActive = true
-        deviceImageView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25).isActive = true
+        deviceImageView.widthAnchor.constraint(equalToConstant: deviceImageView.image!.size.width).isActive = true
+        deviceImageView.heightAnchor.constraint(equalToConstant: deviceImageView.image!.size.height).isActive = true
     }
     
     func setupSegmentedConroller() {
@@ -111,6 +193,34 @@ class DeviceViewController: UIViewController {
         deviceSegmentedConroller.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         deviceSegmentedConroller.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85).isActive = true
         deviceSegmentedConroller.heightAnchor.constraint(equalToConstant: 30).isActive = true
+    }
+    
+    func setupStorageLabel() {
+        storageLabel.topAnchor.constraint(equalTo: deviceSegmentedConroller.bottomAnchor, constant: 20).isActive = true
+        storageLabel.leadingAnchor.constraint(equalTo: deviceSegmentedConroller.leadingAnchor, constant: 20).isActive = true
+        storageLabel.widthAnchor.constraint(equalToConstant: 85).isActive = true
+        storageLabel.heightAnchor.constraint(equalToConstant: 25).isActive = true
+    }
+    
+    func setupStoragePicker() {
+        storageTextField.topAnchor.constraint(equalTo: deviceSegmentedConroller.bottomAnchor, constant: 15).isActive = true
+        storageTextField.trailingAnchor.constraint(equalTo: deviceSegmentedConroller.trailingAnchor, constant: -20).isActive = true
+        storageTextField.widthAnchor.constraint(equalToConstant: 85).isActive = true
+        storageTextField.heightAnchor.constraint(equalToConstant: 35).isActive = true
+    }
+    
+    func setupPriceLabel() {
+        priceLabel.topAnchor.constraint(equalTo: storageLabel.bottomAnchor, constant: 20).isActive = true
+        priceLabel.leadingAnchor.constraint(equalTo: storageLabel.leadingAnchor).isActive = true
+        priceLabel.widthAnchor.constraint(equalToConstant: 110).isActive = true
+        priceLabel.heightAnchor.constraint(equalToConstant: 25).isActive = true
+    }
+    
+    func setupTotalPriceLabel() {
+        totalPriceLabel.topAnchor.constraint(equalTo: priceLabel.topAnchor).isActive = true
+        totalPriceLabel.trailingAnchor.constraint(equalTo: storageTextField.trailingAnchor).isActive = true
+        totalPriceLabel.widthAnchor.constraint(equalToConstant: 160).isActive = true
+        totalPriceLabel.heightAnchor.constraint(equalToConstant: 25).isActive = true
     }
     
     @objc func changeImage(sender: UISegmentedControl) {
@@ -123,6 +233,50 @@ class DeviceViewController: UIViewController {
         let segmentIndex = sender.selectedSegmentIndex
         if segmentIndex >= 0 && segmentIndex < images.count {
             deviceImageView.image = images[segmentIndex]
+        }
+    }
+    
+    // done батн для пикера
+    @objc func donePicker(_ sender: UIPickerView) {
+        self.view.endEditing(true)
+    }
+    
+    // cancel батн для пикера
+    @objc func cancelPicker() {
+        // убрать пикер по кнопке cancel
+        self.view.endEditing(true)
+    }
+    
+}
+
+
+extension DeviceViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView.tag == 0 {
+            return device!.storagePrices.count
+        }
+        return 0
+    }
+}
+
+extension DeviceViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView.tag == 0 {
+            let keys = Array(device!.storagePrices.keys).sorted()
+            return String(keys[row]) + " гб"
+        }
+        return ""
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView.tag == 0 {
+            let keys = Array(device!.storagePrices.keys).sorted()
+            storageTextField.text = String(keys[row]) + " гб"
+            totalPriceLabel.text = String(device!.storagePrices[keys[row]]!) + " руб."
         }
     }
 }
--- ShopApp/ShopApp/UIExtensions.swift
+++ ShopApp/ShopApp/UIExtensions.swift
@@ -81,3 +81,23 @@ extension UIButton {
         )
     }
 }
+
+
+extension UIImage {
+    // измененяет размер изображения и возвращает измененный вариант
+    func resize(size: CGSize) -> UIImage {
+        let widthRatio  = size.width/self.size.width
+        let heightRatio = size.height/self.size.height
+        var updateSize = size
+        if(widthRatio > heightRatio) {
+            updateSize = CGSize(width:self.size.width*heightRatio, height:self.size.height*heightRatio)
+        } else if heightRatio > widthRatio {
+            updateSize = CGSize(width:self.size.width*widthRatio,  height:self.size.height*widthRatio)
+        }
+        UIGraphicsBeginImageContextWithOptions(updateSize, false, UIScreen.main.scale)
+        self.draw(in: CGRect(origin: .zero, size: updateSize))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
+}
